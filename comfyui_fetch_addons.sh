ca0eab04030c8b3391ee75323742b8df5a2dd578
whatifiworked <whatifiworked@gmail.com>
2026-05-21 10:05:56 -0400

    Banner Drafted
    Made a banner, first draft.

---
+++ comfyui_fetch_addons.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if true; then
+    clear
+    cat << 'EOF'
+
+|| =================================================================== ||
+|| ******************************************************************* ||
+|| ******************************************************************* ||
+||     :::    ::::::::: :::::::::               :::::::: ::::    :::   ||
+||   :+: :+:  :+:    :+::+:    :+:             :+:    :+::+:+:   :+:   ||
+||  +:+   +:+ +:+    +:++:+    +:+             +:+    +:+:+:+:+  +:+   ||
+|| +#++:++#++:+#+    +:++#+    +:++#++:++#++:+++#+    +:++#+ +:+ +#+   ||
+|| +#+     +#++#+    +#++#+    +#+             +#+    +#++#+  +#+#+#   ||
+|| #+#     #+##+#    #+##+#    #+#             #+#    #+##+#   #+#+#   ||
+|| ###     ############ #########               ######## ###    ####   ||
+|| ******************************************************************* ||
+|| ******************************************************************* ||
+|| ---Installer for:'ComfyUI' (github  manual install), a subscript of ||  
+||-------------------------------------------------------------------  ||
+|| ComfyUI Setter Upper                                                ||
+|| designed for:                                                       ||    
+||    Operating System:                                                ||
+||                       Fedora 44 (Should work on 42+)                ||
+||    Hardware tested for:   Intel Arc iGPU (CoreUltra)                ||
+||                           Tested on:                                ||
+||                                 Asus Vivobook Copilot+              ||
+||                                 Intel Core Ultra 7 258v/140v        ||
+||                                 32gb total RAM, 16gb/16gb Shared    || 
+||                           Should work on any system using a RHEL    ||
+||                           like distro and using Intel Core Ultra    ||
+||                           processor and having sufficent RAM.       ||
+||                                                                     ||
+||                                                                     ||
+||                                                                     ||
+||  This is a bash script to grab my favorite AddOns for ComfyUI,      ||
+|| place them correctly and install all deps, the generate a clean     ||
+|| report  (another external script) to show vitals regarding          ||
+|| things such as system health                                        ||
+\\ ------------------------------------------------------------------- //
+ 
+ EOF
+    fi   
+
+
